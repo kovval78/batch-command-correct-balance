7b56691d9aa2017a2a7e77a7dbbc72328d6ac1c6
Kamil Kowalczyk <kkowalczyk@ifxpayments.com>
2021-07-06 16:20:39 +0200

    Remove constants from RowParserInterface

--- src/Service/RowParserInterface.php
+++ src/Service/RowParserInterface.php
@@ -5,10 +5,6 @@ namespace App\Service;
 
 interface RowParserInterface
 {
-    public const COLUMN_NUMBER_AMOUNT = 1;
-    public const COLUMN_NUMBER_CURRENCY = 2;
-    public const COLUMN_NUMBER_REFERENCE = 5;
-
     public static function getAmount($line): string;
 
     public static function getCurrency($line): string;
