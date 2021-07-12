236762ae78c8f56fa4bcdc94516d6c29c80666c6
Kamil Kowalczyk <kkowalczyk@ifxpayments.com>
2021-07-12 09:14:07 +0200

    Change ContentProcessorTest.php file

--- src/Service/ContentProcessor.php
+++ src/Service/ContentProcessor.php
@@ -8,11 +8,13 @@ class ContentProcessor implements ContentProcessorInterface
 {
     public function process(array $content): string
     {
+        $rowParser = new RowParser();
+
         $newContent = '';
         foreach ($content as $row) {
-            $amount = RowParser::getAmount($row);
-            $currency = RowParser::getCurrency($row);
-            $reference = RowParser::getReference($row);
+            $amount = $rowParser->getAmount($row);
+            $currency = $rowParser->getCurrency($row);
+            $reference = $rowParser->getReference($row);
             $newContent .= sprintf("bin/console cbs:command:correct-balance ffddb369-8b3b-4cb1-9501-6fa56201fe7c 231884 36488655 %s %s \"%s\"",
                 $amount,
                 $currency,
@@ -20,8 +22,6 @@ class ContentProcessor implements ContentProcessorInterface
             );
             $newContent .= PHP_EOL;
         }
-
-        var_dump($newContent);
 
         return $newContent;
     }
--- tests/Service/ContentProcessorTest.php
+++ tests/Service/ContentProcessorTest.php
@@ -9,34 +9,23 @@ use PHPUnit\Framework\TestCase;
 
 final class ContentProcessorTest extends TestCase
 {
-    private ContentProcessor $contentProcessor;
-
-    public function setUp(): void
+    public function testIsItProcessesValidNewContent()
     {
-        parent::setUp();
-        $this->contentProcessor = new ContentProcessor();
+        $content = [
+            'Anisa Toland',
+            '1,511.48',
+            'GBP',
+            'OF inv 16422449',
+            'MPSS409759-R1799513',
+            'IFX - TRADE - Payment cancelled - Anisa Toland - OF inv 16422449 - MPSS409759-R1799513 - sell GBP 1511.48 @ 1'
+        ];
+
+        $contentProcessor = new ContentProcessor();
+
+        $result = $contentProcessor->process([$content]);
+
+        $expected = 'bin/console cbs:command:correct-balance ffddb369-8b3b-4cb1-9501-6fa56201fe7c 231884 36488655 1511.48 GBP "IFX - TRADE - Payment cancelled - Anisa Toland - OF inv 16422449 - MPSS409759-R1799513 - sell GBP 1511.48 @ 1"' . PHP_EOL;
+
+        $this->assertSame($expected, $result);
     }
-
-    /**
-     * @dataProvider contentProvider
-     */
-    public function testIsNewContentReturnsValidString($content)
-    {
-        $expected = 'bin/console cbs:command:correct-balance ffddb369-8b3b-4cb1-9501-6fa56201fe7c 231884 36488655 Amount CCY "Reference"';
-
-        $this->assertSame($expected, $this->contentProcessor->process($content));
-    }
-
-    public function contentProvider(): array
-    {
-        return array(
-            "Beneficiary",
-            "Amount",
-            "CCY",
-            "Reference",
-            "Ticket Number",
-            "Reference"
-        );
-    }
-
 }
